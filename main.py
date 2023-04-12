cdf981903ccd2ce340c1a008ab1462fa093e789d
jwong65 <jason.wong6507@gmail.com>
2023-04-11 21:45:05 -0400

    Added mult_list function

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ def max_num(*args):
             largest = i
     return largest
 # print(max_num(1, 7, 4))
+
+def mult_list(list):
+    product = 1
+    for i in list:
+        product = i*product
+    return product
+# print(mult_list([1,3,5]))
+# print(mult_list([3,5,0]))
